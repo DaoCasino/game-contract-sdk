a3d39cec10cd116129516fa38e947b0756e633ec
Mikhail <sadsnake@mixbytes.io>
2020-04-27 13:56:38 +0300

    [DPM-127] Remove test.sh additional code

--- cicd/test.sh
+++ cicd/test.sh
@@ -4,36 +4,6 @@ set -o pipefail
 
 . "${BASH_SOURCE[0]%/*}/utils.sh"
 
-is_debug=y
-
-usage() {
-  echo "Test contracts."
-  echo
-  echo "Usage:"
-  echo
-  echo "  $PROGNAME [ OPTIONS ]"
-  echo
-  echo "Options:"
-  echo
-  echo "  --release              : is release"
-  echo
-  echo "  -h, --help           : print this message"
-}
-
-OPTS="$( getopt -o "h" -l "\
-release,\
-help" -n "$PROGNAME" -- "$@" )"
-eval set -- "$OPTS"
-while true; do
-  case "${1:-}" in
-  (--release)      is_debug=n      ; shift   ; readonly is_debug ;;
-  (-h|--help)      usage ; exit 0 ;;
-  (--)             shift ; break ;;
-  (*)              die "Invalid option: ${1:-}." ;;
-  esac
-done
-unset OPTS
-
 log "=========== Running unit tests ==========="
 
 (
