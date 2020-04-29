546eeef4db35c24c762cbf29a4e11fd83f13c298
Mikhail <sadsnake@mixbytes.io>
2020-04-29 08:27:02 +0300

    [DPM-128] Update test.sh

--- cicd/test.sh
+++ cicd/test.sh
@@ -10,6 +10,6 @@ log "=========== Running unit tests ==========="
     tests=$(find ./build/examples/*/tests/ -maxdepth 1 -name "*_unit_test")
     for unit_test in $tests; do
         log "Running $unit_test..."
-        $unit_test
+        $unit_test $@
     done
 )
