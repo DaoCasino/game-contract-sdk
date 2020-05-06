5fa41d206e5d599f956b8cd104daa06150c6506f
Mikhail <sadsnake@mixbytes.io>
2020-05-06 16:25:12 +0300

    [DPM-129] Update code of multi_stub

--- examples/multi_stub/contracts/include/multi_stub/multi_stub.hpp
+++ examples/multi_stub/contracts/include/multi_stub/multi_stub.hpp
@@ -7,21 +7,37 @@ namespace multi_stub {
 using eosio::checksum256;
 using eosio::name;
 
+using session_id_t = uint64_t;
+
 // simple stub game
 class [[eosio::contract]] multi_stub : public game_sdk::game {
   public:
-    static constexpr uint16_t stub_game_action_type{0u};
+    static constexpr uint16_t first_action{0u};
+    static constexpr uint16_t second_action{1u};
+    static constexpr uint16_t third_action{2u};
+
+    struct [[eosio::table("roll")]] roll_row {
+        uint64_t ses_id;
+        std::vector<game_sdk::param_t> counter;
+
+        uint64_t primary_key() const { return ses_id; }
+    };
+    using roll_table = eosio::multi_index<"roll"_n, roll_row>;
 
   public:
-    multi_stub(name receiver, name code, eosio::datastream<const char*> ds) : game(receiver, code, ds) {}
+    multi_stub(name receiver, name code, eosio::datastream<const char*> ds)
+        : game(receiver, code, ds), rolls(_self, _self.value) {}
 
-    virtual void on_new_game(uint64_t ses_id) final;
+    virtual void on_new_game(session_id_t ses_id) final;
 
-    virtual void on_action(uint64_t ses_id, uint16_t type, std::vector<game_sdk::param_t> params) final;
+    virtual void on_action(session_id_t ses_id, uint16_t type, std::vector<game_sdk::param_t> params) final;
 
-    virtual void on_random(uint64_t ses_id, checksum256 rand) final;
+    virtual void on_random(session_id_t ses_id, checksum256 rand) final;
 
-    virtual void on_finish(uint64_t ses_id) final;
+    virtual void on_finish(session_id_t ses_id) final;
+
+  private:
+    roll_table rolls;
 };
 
-} // namespace stub
+} // namespace multi_stub
--- examples/multi_stub/contracts/src/multi_stub.cpp
+++ examples/multi_stub/contracts/src/multi_stub.cpp
@@ -1,26 +1,61 @@
 #include <multi_stub/multi_stub.hpp>
 
 namespace multi_stub {
+using boost::hana::detail::accumulate;
 
 void multi_stub::on_new_game(uint64_t ses_id) {
+
+    rolls.emplace(get_self(), [&](auto& row) {
+        row.ses_id = ses_id;
+        row.counter.resize(6);
+    });
+
     // always require `stub` action
-    require_action(stub_game_action_type);
+    require_action(first_action);
 }
 
 void multi_stub::on_action(uint64_t ses_id, uint16_t type, std::vector<game_sdk::param_t> params) {
-    // always require random
-    require_random();
+    if (type != first_action && type != second_action && type != third_action)
+        return eosio::check(false, "Wrong action type.");
+
+    if (params.size() != 1)
+        return eosio::check(false, "Wrong params size.");
+
+    const auto it = rolls.find(ses_id);
+    eosio::check(it != rolls.end(), "Session error");
+    rolls.modify(it, get_self(), [&](auto& row) { row.counter.push_back(params[0]); });
+
+    return require_random();
 }
 
 void multi_stub::on_random(uint64_t ses_id, checksum256 rand) {
-    // player always lost
-    finish_game(zero_asset);
+    uint8_t stored;
+
+    const auto it = rolls.find(ses_id);
+    eosio::check(it != rolls.end(), "Session error");
+    rolls.modify(it, get_self(), [&](auto& row) {
+        row.counter.push_back(service::cut_to<game_sdk::param_t>(rand));
+        stored = row.counter.size();
+    });
+
+    switch (stored) {
+    case 2:
+        return require_action(second_action);
+    case 4:
+        return require_action(third_action);
+    case 6:
+        return finish_game(zero_asset);
+    default:
+        return eosio::check(false, "Unreachable.");
+    }
 }
 
 void multi_stub::on_finish(uint64_t ses_id) {
-    // do nothing
+    if (const auto roll_itr = rolls.find(ses_id); roll_itr != rolls.end()) {
+        rolls.erase(roll_itr);
+    }
 }
 
-} // namespace stub
+} // namespace multi_stub
 
 GAME_CONTRACT(multi_stub::multi_stub)
