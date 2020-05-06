73bad1aae951c638b582e8be1ec170b47c72b051
Mikhail <sadsnake@mixbytes.io>
2020-05-06 14:12:14 +0300

    [DPM-129] Update multi stub

---
+++ examples/multi_stub/contracts/include/multi_stub/multi_stub.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <game-contract-sdk/game_base.hpp>
+
+namespace multi_stub {
+
+using eosio::checksum256;
+using eosio::name;
+
+// simple stub game
+class [[eosio::contract]] multi_stub : public game_sdk::game {
+  public:
+    static constexpr uint16_t stub_game_action_type{0u};
+
+  public:
+    multi_stub(name receiver, name code, eosio::datastream<const char*> ds) : game(receiver, code, ds) {}
+
+    virtual void on_new_game(uint64_t ses_id) final;
+
+    virtual void on_action(uint64_t ses_id, uint16_t type, std::vector<game_sdk::param_t> params) final;
+
+    virtual void on_random(uint64_t ses_id, checksum256 rand) final;
+
+    virtual void on_finish(uint64_t ses_id) final;
+};
+
+} // namespace stub
